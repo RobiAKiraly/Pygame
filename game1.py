f62499861192de33ac4a34faf2a6da9377b90387
RobiAKiraly <129603776+RobiAKiraly@users.noreply.github.com>
2023-04-01 17:20:05 +0300

    Update game1.py

--- game1.py
+++ game1.py
@@ -4,12 +4,21 @@ from pygame import *
 
 window = display.set_mode((700, 500))
 display.set_caption("First application")
+window.fill((255, 100, 50))
+background = transform.scale(image.load("fail_1.jpg"), (700,500))
+window.blit(background,(0,0))
 
-height=60
+'''height=60
 width=40
 x=5
 y=500-height-5
 draw.rect(window,(0,0,255),(x,y,width,height))
+'''
+x=100
+y=100
+merlin = transform.scale(image.load('shutterstock_783589810_frame-32.png'), (100, 100))
+window.blit(merlin, (x, y))
+
 display.update()
 
 run=True
@@ -19,6 +28,7 @@ while run:
         if e.type == KEYDOWN:
             if e.key==K_LEFT:
                 print('left')
+                
             elif e.key==K_RIGHT:
                 print('right')
             elif e.key==K_UP:
